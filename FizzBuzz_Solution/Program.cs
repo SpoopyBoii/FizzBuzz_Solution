dac42cbcdd21220fe133f3665a2998efcfc08f86
Lucien van Wyk <89403985+SpoopyBoii@users.noreply.github.com>
2022-06-01 12:56:13 +0200

    Uploading entire project
    Uploading all files to GitHub.

---
+++ FizzBuzz_Solution/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz_Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("====================================");
+			for (int n = 1; n <= 100; n++)
+			{
+				if (n % 3 == 0 && n % 5 == 0)
+				{
+					Console.WriteLine("FizzBuzz");
+				}
+				else if (n % 3 == 0)
+				{
+					Console.WriteLine("Fizz");
+				}
+				else if (n % 5 == 0)
+				{
+					Console.WriteLine(n + " Buzz");
+				}
+				else
+				{
+					Console.WriteLine(n);
+				}
+			}
+			Console.WriteLine("====================================");
+			Console.WriteLine("Press any key to exit the program...");
+			Console.WriteLine("====================================");
+			Console.ReadLine();
+		}
+    }
+}
